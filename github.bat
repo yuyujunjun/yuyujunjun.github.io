6c655071f710c72e2f2cd40b70c3119444886045
yuyujunjun <fcsx1tf@163.com>
2018-08-23 20:55:47 +0800

    upload blog

---
+++ github.bat
@@ -0,0 +1,6 @@
+mv  ./tags ../tags
+mv ../yuyujunjun-pages/tag ./tag
+mv ../yuyujunjun-pages/author ./author
+git add ./
+git commit -m "upload blog"
+git push origin master
---
+++ local.bat
@@ -0,0 +1,3 @@
+mv ../tags ./tags
+rm -rf  ./author  ./tag sitemap.xml
+bundle exec jekyll serve

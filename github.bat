d5176a577a303f9df70715e4e0b0a7ba792b7406
yuyujunjun <fcsx1tf@163.com>
2018-08-24 00:14:40 +0800

    upload blog

--- github.bat
+++ github.bat
@@ -1,7 +1,7 @@
 mv  ./tags ../tags
-mv ../yuyujunjun-pages/tag ./tag
-mv ../yuyujunjun-pages/author ./author
-mv ../yuyujunjun-pages/sitemap.xml ./sitemap.xml
+cp ../yuyujunjun-pages/tag ./tag
+cp ../yuyujunjun-pages/author ./author
+cp ../yuyujunjun-pages/sitemap.xml ./sitemap.xml
 git add ./
 git commit -m "upload blog"
 git push origin master

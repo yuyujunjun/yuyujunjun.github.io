dedda17716dc2b5b0c6dc466e717c7facfbd74ff
yuyujunjun <fcsx1tf@163.com>
2018-08-25 01:28:41 +0800

    upload blog

--- github.bat
+++ github.bat
@@ -1,6 +1,6 @@
 move  ./tags ../tags
-move ../yuyujunjun-pages/tag ./tag
-move ../yuyujunjun-pages/author ./author
+move ../yuyujunjun-pages/tag/* ./tag/*
+move ../yuyujunjun-pages/author/* ./author/*
 move ../yuyujunjun-pages/sitemap.xml ./sitemap.xml
 git add ./
 git commit -m "upload blog"
